52ff7ac103b8ea734932b24d053d69f2b9146e8c
Lyubomir Stamenov <lyubomir.s@appgr8.com>
2024-01-08 21:58:56 +0200

    WTT-1: wip

---
+++ src/db/index.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import {
+  dbType,
+  dbHost,
+  dbPort,
+  dbUsername,
+  dbPassword,
+  dbName,
+} from '../config';
+import { User } from '../entities';
+
+const databaseConfig: TypeOrmModule = {
+  type: dbType,
+  host: dbHost,
+  port: dbPort,
+  username: dbUsername,
+  password: dbPassword,
+  database: dbName,
+  entities: [User],
+  synchronize: true,
+};
+
+export default TypeOrmModule.forRoot(databaseConfig);
