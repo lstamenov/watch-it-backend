4b40c1876437050a15d40d859c6023302efb086f
Lyubomir Stamenov <lyubomir.s@appgr8.com>
2024-01-14 16:36:11 +0200

    WTT-5: added apis for updating password and avatar

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -13,4 +13,22 @@ class UserDto {
   password: string;
 }
 
+class UpdateUserAvatar {
+  @IsString()
+  @IsNotEmpty()
+  avatarURL: string;
+}
+
+class UpdateUserPassword {
+  @IsString()
+  @IsNotEmpty()
+  oldPassword: string;
+
+  @IsString()
+  @IsNotEmpty()
+  newPassword: string;
+}
+
+export { UpdateUserAvatar, UpdateUserPassword };
+
 export default UserDto;
