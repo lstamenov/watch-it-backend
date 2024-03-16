49d7da6a9f2f8566bc371f93ccf269a05dc44aa7
Lyubomir Stamenov <lyubomir.stamenov@dss.bg>
2024-03-16 10:44:38 +0200

    added top rated shows endpoint

--- src/controllers/show.controller.ts
+++ src/controllers/show.controller.ts
@@ -35,6 +35,18 @@ class ShowController {
       return res.status(HttpStatus.NOT_FOUND).send(e?.message);
     }
   }
+
+  @Get('/top-rated')
+  public async getTopRatedShows(@Req() req: Request, @Res() res: Response) {
+    try {
+      const language: string = req.query.language.toString();
+      const shows: Show[] = await this.showService.getTopRatedShows(language);
+
+      return res.status(HttpStatus.OK).send(shows);
+    } catch (e) {
+      return res.status(HttpStatus.NOT_FOUND).send(e?.message);
+    }
+  }
 }
 
 export default ShowController;
