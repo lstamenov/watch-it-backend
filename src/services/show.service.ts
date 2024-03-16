504981e5e46cd61da0dd3a61f2534a3b89ac73e0
Lyubomir Stamenov <lyubomir.stamenov@dss.bg>
2024-03-16 10:36:45 +0200

    added popular shows endpoint

--- src/services/show.service.ts
+++ src/services/show.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import FetchService from './fetch.service';
 import { Show } from 'src/entities';
 import { showUrl } from 'src/config';
+import { ShowsApiReponse } from 'src/types/interfaces';
 
 @Injectable()
 class ShowService extends FetchService {
@@ -17,6 +18,21 @@ class ShowService extends FetchService {
     const queryParams = { language };
 
     return this.get({ path, queryParams });
+  }
+
+  public async getFullDetailedShows(content: Show[], language: string): Promise<Show[]> {
+    const fullDetailedShowsResponse: Promise<Show[]> = Promise.all(
+      content.map((movie) => this.getShowById(movie.id, language)),
+    );
+    return fullDetailedShowsResponse;
+  }
+
+  public async getPopularShows(language: string): Promise<Show[]> {
+    const path: string = `${this.basePath}/popular`;
+    const queryParams = { language };
+    const popularShowsResponse = await this.get<ShowsApiReponse>({ path, queryParams });
+
+    return this.getFullDetailedShows(popularShowsResponse.results, language);
   }
 }
 
